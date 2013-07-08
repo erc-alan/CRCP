2611ebe384063624696ab5f76be402c3b6c52804
alan <alan@e-reverse.com>
2013-07-08 09:45:20 +0900

    first version of crcp

---
+++ services/CRCPTESTDB/src/com/crcptestdb/data/Gyosha.java
@@ -0,0 +1,32 @@
+
+package com.crcptestdb.data;
+
+
+
+/**
+ *  CRCPTESTDB.Gyosha
+ *  07/01/2013 14:23:08
+ * 
+ */
+public class Gyosha {
+
+    private String gyoshaCd;
+    private String address;
+
+    public String getGyoshaCd() {
+        return gyoshaCd;
+    }
+
+    public void setGyoshaCd(String gyoshaCd) {
+        this.gyoshaCd = gyoshaCd;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
---
+++ services/CRCPTESTDB/src/com/crcptestdb/data/Jishahaikibutsu.java
@@ -0,0 +1,59 @@
+
+package com.crcptestdb.data;
+
+
+
+/**
+ *  CRCPTESTDB.Jishahaikibutsu
+ *  07/01/2013 14:23:08
+ * 
+ */
+public class Jishahaikibutsu {
+
+    private String jishahaikibutsuCd;
+    private String jishahaikibutsuName;
+    private String jishahaikiCategory;
+    private String jwnHaikibutsuName;
+    private String tokkanbutsuFlag;
+
+    public String getJishahaikibutsuCd() {
+        return jishahaikibutsuCd;
+    }
+
+    public void setJishahaikibutsuCd(String jishahaikibutsuCd) {
+        this.jishahaikibutsuCd = jishahaikibutsuCd;
+    }
+
+    public String getJishahaikibutsuName() {
+        return jishahaikibutsuName;
+    }
+
+    public void setJishahaikibutsuName(String jishahaikibutsuName) {
+        this.jishahaikibutsuName = jishahaikibutsuName;
+    }
+
+    public String getJishahaikiCategory() {
+        return jishahaikiCategory;
+    }
+
+    public void setJishahaikiCategory(String jishahaikiCategory) {
+        this.jishahaikiCategory = jishahaikiCategory;
+    }
+
+    public String getJwnHaikibutsuName() {
+        return jwnHaikibutsuName;
+    }
+
+    public void setJwnHaikibutsuName(String jwnHaikibutsuName) {
+        this.jwnHaikibutsuName = jwnHaikibutsuName;
+    }
+
+    public String getTokkanbutsuFlag() {
+        return tokkanbutsuFlag;
+    }
+
+    public void setTokkanbutsuFlag(String tokkanbutsuFlag) {
+        this.tokkanbutsuFlag = tokkanbutsuFlag;
+    }
+
+}
